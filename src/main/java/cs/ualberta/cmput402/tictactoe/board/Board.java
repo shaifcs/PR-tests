e9e3143cdb6cb4fe67e507db76a5acca53c13dbe
msacbd@gmail.com <msacbd@gmail.com>
2019-01-16 15:37:53 -0700

    for testing pull requests with different branch

---
+++ src/main/java/cs/ualberta/cmput402/tictactoe/board/Board.java
@@ -0,0 +1,132 @@
+package cs.ualberta.cmput402.tictactoe.board;
+
+import cs.ualberta.cmput402.tictactoe.board.exceptions.InvalidMoveException;
+
+/**
+ * Created by snadi on 2018-07-16.
+ */
+public class Board {
+
+    public enum Player {X, O, NONE};
+    private Player currentPlayer;
+    private Player winner;
+    private Player board[][];
+
+    public Board(){
+        board = new Player[3][3];
+        initBoard();
+        winner = null;
+        currentPlayer = Player.X;
+    }
+
+    private void initBoard(){
+        for (int i = 0; i < 3; i++)
+            for(int j = 0; j < 3; j++)
+                board[i][j] = Player.NONE;
+
+    }
+
+    public void playMove(int row, int col) throws InvalidMoveException {
+
+        if(row < 0 || row >= 3 || col <0 || col >=3)
+            throw new InvalidMoveException("Input coordinates are outside the board. Try again");
+
+        if(!isSquareAvailable(row, col)){
+            //the given coordinates already contain a played move
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("Invalid Move: Square ");
+            stringBuilder.append(row);
+            stringBuilder.append(",");
+            stringBuilder.append(col);
+            stringBuilder.append(" already contains ");
+            stringBuilder.append(getSymbol(board[row][col]));
+            throw new InvalidMoveException(stringBuilder.toString());
+        }else{
+            board[row][col] = currentPlayer;
+
+            if (hasWon(row, col))
+                winner = currentPlayer;
+            else if(currentPlayer == Player.X)
+                currentPlayer = Player.O;
+            else
+                currentPlayer = Player.X;
+        }
+
+    }
+
+
+    private boolean isSquareAvailable(int row, int col){
+        return (board[row][col] != Player.X && board[row][col] != Player.O);
+    }
+
+    public String getSymbol(Player player){
+        switch(player){
+            case X:
+                return "X";
+            case O:
+                return "O";
+            case NONE:
+                return " ";
+            default:
+                return "UNKNOWN SYMBOL";
+        }
+    }
+
+    public boolean hasWon(int lastColPlayed, int lastRowPlayed){
+
+        //check horizontal
+        if (board[lastColPlayed][0].equals(board[lastColPlayed][1]) && board[lastColPlayed][1].equals(board[lastColPlayed][2])){
+            return true;
+        }
+        //check vertical
+        else if(board[0][lastRowPlayed].equals(board[1][lastRowPlayed]) && board[1][lastRowPlayed].equals(board[2][lastRowPlayed])){
+            return true;
+        }
+        //check diagonal
+        else{
+            if(isOnRightDiag(lastColPlayed, lastRowPlayed) && board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]))
+                return true;
+            else if (isOnLeftDiag(lastColPlayed, lastRowPlayed) && board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0]))
+                return true;
+        }
+
+        return false;
+    }
+
+    private boolean isOnRightDiag(int col, int row){
+        return (col == 0 && row == 0) || (col == 1 && row == 1) || (col == 2 & row == 2);
+    }
+
+    private boolean isOnLeftDiag(int col, int row){
+        return (col == 0 && row == 2) || (col == 1 && row == 1) || (col == 2 & row == 0);
+    }
+
+    public void printBoard(){
+        for(int i  = 0; i < 3; i++){
+            for(int j = 0 ; j < 3; j++){
+
+               System.out.print(getSymbol(board[i][j]));
+
+                if (j == 2)
+                    System.out.println("");
+                else
+                    System.out.print(" | ");
+            }
+            System.out.println("----------");
+        }
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public Player getWinner() {
+        return winner;
+    }
+
+    public Player getPlayerAtPos(int row, int col){
+        return board[row][col];
+    }
+
+
+}
---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,180 @@
+import cs.ualberta.cmput402.tictactoe.board.Board;
+import cs.ualberta.cmput402.tictactoe.board.exceptions.InvalidMoveException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runners.model.TestClass;
+
+/**
+ * Created by snadi on 2018-07-23.
+ */
+public class BoardTest {
+
+    private Board board;
+
+    @Before
+    public void setup() {
+        board = new Board();
+    }
+
+    @Test
+    public void testInit() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                assert (board.getPlayerAtPos(i, j).equals(Board.Player.NONE));
+            }
+        }
+
+        assert (board.getCurrentPlayer().equals(Board.Player.X));
+        assert (board.getWinner() == null);
+    }
+
+    @Test
+    public void testPlayMoveEmptyBoard() throws InvalidMoveException {
+
+        assert (board.getPlayerAtPos(1, 1).equals(Board.Player.NONE));
+        board.playMove(1, 1);
+
+        assert (board.getPlayerAtPos(1, 1).equals(Board.Player.X));
+        assert (board.getCurrentPlayer().equals(Board.Player.O));
+
+        assert (board.getPlayerAtPos(0, 0).equals(Board.Player.NONE));
+        board.playMove(0, 0);
+        assert (board.getPlayerAtPos(0, 0).equals(Board.Player.O));
+        assert (board.getCurrentPlayer().equals(Board.Player.X));
+    }
+
+
+    @Test(expected = InvalidMoveException.class)
+    public void testPlayMoveInvalidRowPosition() throws InvalidMoveException {
+        board.playMove(3, 0);
+    }
+
+    @Test(expected = InvalidMoveException.class)
+    public void testPlayMoveInvalidColPosition() throws InvalidMoveException {
+        board.playMove(0, 3);
+    }
+
+    @Test(expected = InvalidMoveException.class)
+    public void testPlayMoveInvalidNegRowPosition() throws InvalidMoveException {
+        board.playMove(-1, 2);
+    }
+
+    @Test(expected = InvalidMoveException.class)
+    public void testPlayMoveInvalidNegColPosition() throws InvalidMoveException {
+        board.playMove(2, -1);
+    }
+
+    @Test(expected = InvalidMoveException.class)
+    public void testPlayMoveExistinSq() throws InvalidMoveException {
+        board.playMove(2, 1);
+        board.playMove(2, 1);
+    }
+
+    @Test
+    public void testGetSymbol() {
+        assert (board.getSymbol(Board.Player.NONE).equals(" "));
+        assert (board.getSymbol(Board.Player.X).equals("X"));
+        assert (board.getSymbol(Board.Player.O).equals("O"));
+    }
+
+
+    @Test
+    public void testWinRightDiagonalX() throws InvalidMoveException {
+        board.playMove(0,0); //player X
+        board.playMove(0,1);
+        board.playMove(1,1); //player X
+        board.playMove(2,1);
+        board.playMove(2,2); //player X
+
+        assert(board.getWinner().equals(Board.Player.X));
+    }
+
+    @Test
+    public void testWinRightDiagonalO() throws InvalidMoveException {
+
+        board.playMove(0,1);
+        board.playMove(1,1); //player 0
+        board.playMove(2,1);
+        board.playMove(2,2); //player 0
+        board.playMove(1,0);
+        board.playMove(0,0); //player 0
+
+        assert(board.getWinner().equals(Board.Player.O));
+    }
+
+    @Test
+     public void testWinLeftDiagonalO() throws InvalidMoveException {
+
+        board.playMove(1,2);
+        board.playMove(1,1); //player 0
+        board.playMove(2,1);
+        board.playMove(0,2); //player 0
+        board.playMove(0,0);
+        board.playMove(2,0); //player 0
+
+        assert(board.getWinner().equals(Board.Player.O));
+    }
+
+    @Test
+    public void testWinLeftDiagonalX() throws InvalidMoveException {
+
+        board.playMove(1,1); //player X
+        board.playMove(0,1);
+        board.playMove(2,0); //player X
+        board.playMove(1,2);
+        board.playMove(0,2); //player X
+
+        assert(board.getWinner().equals(Board.Player.X));
+    }
+
+    @Test
+      public void testWinHorizonalX() throws InvalidMoveException {
+
+        board.playMove(1,0); //player X
+        board.playMove(2,1);
+        board.playMove(1,1); //player X
+        board.playMove(2,2);
+        board.playMove(1,2); //player X
+
+        assert(board.getWinner().equals(Board.Player.X));
+    }
+
+    @Test
+    public void testWinHorizonalO() throws InvalidMoveException {
+
+        board.playMove(0,1);
+        board.playMove(2,1); //player 0
+        board.playMove(1,1);
+        board.playMove(2,2); //player 0
+        board.playMove(1,0);
+        board.playMove(2,0); //player 0
+
+        assert(board.getWinner().equals(Board.Player.O));
+    }
+
+    @Test
+    public void testWinVerticalX() throws InvalidMoveException {
+
+        board.playMove(0,1); //player X
+        board.playMove(0,2);
+        board.playMove(1,1); //player X
+        board.playMove(2,2);
+        board.playMove(2,1); //player X
+
+        assert(board.getWinner().equals(Board.Player.X));
+    }
+
+    @Test
+    public void testVerticalO() throws InvalidMoveException {
+
+        board.playMove(0,1);
+        board.playMove(2,2); //player 0
+        board.playMove(1,1);
+        board.playMove(1,2); //player 0
+        board.playMove(1,0);
+        board.playMove(0,2); //player 0
+
+        assert(board.getWinner().equals(Board.Player.O));
+    }
+
+}
